7a110b787134d2200f3884c431255f26b9ec6157
Sindre T. Strøm <sindrets@gmail.com>
2017-06-17 17:24:07 +0200

    Initial sync - v1.0.0

---
+++ spec/title-bar-replacer-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TitleBarReplacerView from '../lib/title-bar-replacer-view';
+
+describe('TitleBarReplacerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
